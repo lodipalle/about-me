46f5e356ea983e465fce4f024de3f510ecf3e1bb
lodi palle <lodipalle@gmail.com>
2020-10-16 13:15:46 +1100

    changed article class about to div and coded app.js functionalities

---
+++ app.js
@@ -0,0 +1,19 @@
+const tabs = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener("click", function (e) {
+  // console.log(e.target.dataset.id);
+  const id = e.target.dataset.id;
+  if (id) {
+    tabs.forEach(function (tab) {
+      tab.classList.remove("active");
+      e.target.classList.add("active");
+    });
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
